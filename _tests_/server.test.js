d9e8ffc4787ffee34e74f70547f3df88d7578ed4
dannevernguyens <danguyenuw@gmail.com>
2020-04-16 21:10:14 -0700

    finished writing tests for server and users model

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -13,22 +13,31 @@
 const app = require('../lib/server.js');
 const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(app.server);
+const Model = require('../lib/models/model.js');
+const userSchema = require('../lib/models/user-schema.js');
+const UsersModel = new Model(userSchema);
 
 
 describe('All Routes Work', () => {
   it('POST /signup // user signup works via request.body', async () => {
-
-    expect(false).toBe(true);
+    let userTest = await UsersModel.create({
+      username: 'testUser',
+      password: 'testPass',
+    });
+    
+    expect(userTest.username).toBe('testUser');
   });
 
   it('POST /signin // user signin works', async () => {
+   let response = await (mockRequest.post('/signin')).auth('testUser', 'testPass');
 
-    expect(false).toBe(true);
+    expect(response.status).toBe(200);
   });
 
   it('GET /users // shows all users', async () => {
+    let results = await UsersModel.readByQuery({});
 
-    expect(false).toBe(true);
+    expect(results.length).toBe(1);
   });
 
 });
--- _tests_/users-model.test.js
+++ _tests_/users-model.test.js
@@ -3,32 +3,60 @@
 const app = require('../lib/server.js');
 const supergoose = require('@code-fellows/supergoose');
 const mockRequest = supergoose(app.server);
-const UserModel = require('../lib/models/products/products-model');
+const Model = require('../lib/models/model.js');
+const userSchema = require('../lib/models/user-schema.js');
+const UsersModel = new Model(userSchema);
+
+
+beforeAll(async () => {
+  await UsersModel.create({
+    username: 'testUser1',
+    password: 'testPass1',
+    fname: 'Alpha',
+    lname: 'Bet',
+  });
+
+  await UsersModel.create({
+    username: 'testUser2',
+    password: 'testPass2',
+    fname: 'Beta',
+    lname: 'Fish',
+  });
+});
 
 describe('User CRUD Tests', () => {
-  it('create', async () => {
+  it('can create a new user', async () => {
+    let response = await UsersModel.create({
+      username: 'testcreate',
+      password: 'createpass',
+    });
 
-    expect(false).toBe(true);
+    expect(response).toBeTruthy();
+    expect(response.username).toBe('testcreate');
   });
 
-  it('read all', async () => {
+  it('can read all users', async () => {
+    let results = await UsersModel.readByQuery({});
 
-    expect(false).toBe(true);
+    expect(results.length).toBe(3);
   });
 
-  it('read by ID', async () => {
+  it('can delete a newly created user', async () => {
+    let newUser = await UsersModel.create({
+      username: 'deleteme',
+      password: 'medelete',
+    });
 
-    expect(false).toBe(true);
-  });
-
-  it('update', async () => {
-
-
-    expect(false).toBe(true);
-  });
-
-  it('delete', async () => {
+    let deletedResult = await UsersModel.delete(newUser._id);
     
-    expect(false).toBe(true);
+    expect(deletedResult).toBe(newUser._id);
+  });
+
+  it('can delete an existing user', async () => {
+    let records = await UsersModel.readByQuery({ username: 'testUser1'});
+    expect(records.length).toBe(1);
+    let testId = records[0]._id;
+    let deletedResult = await UsersModel.delete(testId);
+    expect(deletedResult).toBe(testId);
   });
 });
