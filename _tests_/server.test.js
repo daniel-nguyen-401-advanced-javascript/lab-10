654cc2e2289802947cb6d7f9b944b99cff5128ec
dannevernguyens <danguyenuw@gmail.com>
2020-04-16 19:51:06 -0700

    figured out how to add, get, and delete users. got mongo connected

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -1,2 +1,34 @@
 'use strict';
 
+/// test all defined endpoints
+
+/// post/signin
+
+/// post/signup-body
+
+/// post/signup-header
+
+/// get/users
+
+const app = require('../lib/server.js');
+const supergoose = require('@code-fellows/supergoose');
+const mockRequest = supergoose(app.server);
+
+
+describe('All Routes Work', () => {
+  it('POST /signup // user signup works via request.body', async () => {
+
+    expect(false).toBe(true);
+  });
+
+  it('POST /signin // user signin works', async () => {
+
+    expect(false).toBe(true);
+  });
+
+  it('GET /users // shows all users', async () => {
+
+    expect(false).toBe(true);
+  });
+
+});
--- _tests_/users-model.test.js
+++ _tests_/users-model.test.js
@@ -1,2 +1,34 @@
 'use strict';
 
+const app = require('../lib/server.js');
+const supergoose = require('@code-fellows/supergoose');
+const mockRequest = supergoose(app.server);
+const UserModel = require('../lib/models/products/products-model');
+
+describe('User CRUD Tests', () => {
+  it('create', async () => {
+
+    expect(false).toBe(true);
+  });
+
+  it('read all', async () => {
+
+    expect(false).toBe(true);
+  });
+
+  it('read by ID', async () => {
+
+    expect(false).toBe(true);
+  });
+
+  it('update', async () => {
+
+
+    expect(false).toBe(true);
+  });
+
+  it('delete', async () => {
+    
+    expect(false).toBe(true);
+  });
+});
