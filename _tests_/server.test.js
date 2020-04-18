da2a21ab68c43ebbcde6dd6e3e7df539492bef0f
dannevernguyens <danguyenuw@gmail.com>
2020-04-18 09:46:18 -0700

    made some test adjustments after lecture review

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -25,12 +25,27 @@ describe('All Routes Work', () => {
       password: 'testPass',
     });
     
+    //expect status
+    //expect(response.status).toBe(201)
+    //expect(response.body._id).toBeDefined()
+    //expect(response.body.password).toBeDefined()
+    //expect(response.body.password).not.toBe('testPass)
+    //expect pw that doesnt match input pass
+    //expect new id
+    //use mockRequest.post('/signup').send({}) instead of await UsersMode.create({})
     expect(userTest.username).toBe('testUser');
   });
 
   it('POST /signin // user signin works', async () => {
    let response = await (mockRequest.post('/signin')).auth('testUser', 'testPass');
 
+
+
+    //check the response for correct status
+    //check user record of who was signed in
+    //expect(response.body.username).toBe('testUser')
+    //change the res.send('found') to be res.send(req.user)
+    //easier to check response that way
     expect(response.status).toBe(200);
   });
 
